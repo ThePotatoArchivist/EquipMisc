5bf96ceaa7db4f2278f4fbfb690ebfdef4399479
ThePotatoArchivist <josiahfu@gmail.com>
2025-10-12 03:08:56 -0700

    Improve shears loot fix

--- src/main/java/archives/tater/equipmisc/mixin/shears/ItemStackMixin.java
+++ src/main/java/archives/tater/equipmisc/mixin/shears/ItemStackMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.registry.tag.TagKey;
 
 @Mixin(ItemStack.class)
@@ -23,5 +24,13 @@ public abstract class ItemStackMixin {
     )
     private boolean shearCheck(boolean original, @Local(argsOnly = true) Item item) {
         return original || item == Items.SHEARS && isIn(ConventionalItemTags.SHEAR_TOOLS);
+    }
+
+    @ModifyReturnValue(
+            method = "isIn(Lnet/minecraft/registry/entry/RegistryEntryList;)Z",
+            at = @At("RETURN")
+    )
+    private boolean shearCheck(boolean original, @Local(argsOnly = true) RegistryEntryList<Item> list) {
+        return original || list.isBound() && list.size() == 1 && list.get(0).value() == Items.SHEARS && this.isIn(ConventionalItemTags.SHEAR_TOOLS);
     }
 }
